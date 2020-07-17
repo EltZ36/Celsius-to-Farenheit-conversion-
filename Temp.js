d7092be2e48086d1fc4187ba77ce42aebb508b64
Elton <32944003+EltZ36@users.noreply.github.com>
2020-07-17 16:27:02 -0700

    Update Temp.js

--- Temp.js
+++ Temp.js
@@ -11,6 +11,6 @@ function draw() {
     console.log(
       temps[i] + " degrees F which is " + nfc(5 * (temps[i] - 32) / 9, 2) + " degrees C and " + nfc(5 * (temps[i] - 32) / 9 + 273.15, 4) + " Kelvin"
     );
-    i++; // i =i + 1
+    i++; // i = i + 1
   }
 }
