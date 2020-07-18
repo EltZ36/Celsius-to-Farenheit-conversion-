f622cd8c0790c28dfc92cbd36b854e35b8f602e2
Elton <32944003+EltZ36@users.noreply.github.com>
2020-07-17 21:13:36 -0700

    Conversion from Celsius to Fahrenheit
    I want to add another function and put it inside the temperature_convert() function in order to increase readability.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def temperature_convert():
-  Question = int(input("Do you want to convert from Centigrade to Farenheit (1) or Farenheit to Centigrade? (2) "))
+  Question = int(input("Do you want to convert from Centigrade to Fahrenheit (1) or Fahrenheit to Centigrade? (2) "))
   if Question == 1:
-    return True
+    Question = True
+    while Question == True:
+    c = int(input("How many degrees Centigrade?"))
+    F1 = (((c/5)*9)+32)
+    print(str(F1) + ' degrees Centigrade')
+    break
   elif Question == 2:
-    return True 
+   Question = True
+   while Question == True:
+      f = int(input("How many degrees Fahrenheit?"))
+      return f
   else:
     print("Please enter the number next to the option you want.")
     temperature_convert()
