aa5b3049da514d1b00c9d573b50783fc767f7f98
Elton <32944003+EltZ36@users.noreply.github.com>
2020-07-18 14:18:49 -0700

    Update main.py
    Wanted to have a global variable but, that didn't work so I changed it to local variables. I want to do a OOP version for this as I feel there are too many if, elif, and else statements.

--- main.py
+++ main.py
@@ -13,14 +13,24 @@ def T_to_C():
   C1 = (((f-32)*5)/9)
   # Have to convert C1 to a string as it is a float and can't be added with another string.
   print(str(C1) + ' degrees Fahrenheit')
-  Again2 = input("Do you want to convert again? (Y) or (N)")
-  if Again2 == "Y":
+  Again = input("Do you want to convert again? (Y) or (N)")
+  if Again == "Y":
     temperature_convert()
   else:
     return None
 
+ def C_to_K():
+  C2 = int(input("How many degrees Centigrade?"))
+  K = (C2+273.15)
+  print(str(K)+ ' degrees Kelvin')
+  Again = input("Do you want to convert again? (Y) or (N)")
+  if Again == "Y":
+    temperature_convert()
+  else:
+    return None
+  
 def temperature_convert():
-  Question = int(input("Do you want to convert from Centigrade to Fahrenheit (1) or Fahrenheit to Centigrade? (2)"))
+  Question = int(input("Do you want to convert from Centigrade to Fahrenheit (1) , Fahrenheit to Centigrade? (2),  or Centigrade to Kelvin? (3)"))
   if Question == 1: 
     Question = True
     while Question == True:
@@ -33,9 +43,15 @@ def temperature_convert():
     while Question == True:
       T_to_C()
       break
+  elif Question == 3:
+    Question = True
+    while Question == True:
+      C_to_K()
+      break
   else:
-    print("Please type in the number next to the conversion that you want.")
+    print("Please type in the number next to the conversion you want.")
     # Guess this is a form of recursion where the function calls itself again.
     temperature_convert()
+
 print('Welcome!')
 temperature_convert()
