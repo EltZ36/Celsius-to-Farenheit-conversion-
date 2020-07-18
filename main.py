95bfcb9cc9f89bba1c39a0b69324f72de39afe72
Elton <32944003+EltZ36@users.noreply.github.com>
2020-07-17 19:20:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 def temperature_convert():
-  Question = "Do you want to convert from Centigrade to Farenheit or Farenheit to Centigrade?"
+  Question = int(input("Do you want to convert from Centigrade to Farenheit (1) or Farenheit to Centigrade? (2) "))
+  if Question == 1:
+    return True
+  elif Question == 2:
+    return True 
+  else:
+    print("Please enter the number next to the option you want.")
+    temperature_convert()
+temperature_convert()
