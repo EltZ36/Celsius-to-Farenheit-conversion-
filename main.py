0c74ea7cbbd028aa984e1e32ccd4aaad33dde503
Elton <32944003+EltZ36@users.noreply.github.com>
2020-07-17 21:36:13 -0700

    Update main.py
    Want to make it have less if statements and also have a kelvin conversion.

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+def C_to_F():
+  c = int(input("How many degrees Centigrade?"))
+  F1 = (((c/5)*9)+32)
+  print(str(F1) + ' degrees Centigrade')
+  Again = input("Do you want to convert again? (Y) or (N)")
+  if Again == "Y":
+    temperature_convert()
+  else:
+    return None
+
+def T_to_C():
+  f = int(input("How many degrees Fahrenheit?"))
+  C1 = (((f-32)*5)/9)
+  print(str(C1) + ' degrees Fahrenheit')
+  Again2 = input("Do you want to convert again? (Y) or (N)")
+  if Again2 == "Y":
+    temperature_convert()
+  else:
+    return None
+
 def temperature_convert():
-  Question = int(input("Do you want to convert from Centigrade to Fahrenheit (1) or Fahrenheit to Centigrade? (2) "))
-  if Question == 1:
+  Question = int(input("Do you want to convert from Centigrade to Fahrenheit (1) or Fahrenheit to Centigrade? (2)"))
+  if Question == 1: 
     Question = True
     while Question == True:
-    c = int(input("How many degrees Centigrade?"))
-    F1 = (((c/5)*9)+32)
-    print(str(F1) + ' degrees Centigrade')
-    break
+      C_to_F()
+      #Break stops the while loop from looping and starting the conversion over again.
+      break
   elif Question == 2:
-   Question = True
-   while Question == True:
-      f = int(input("How many degrees Fahrenheit?"))
-      return f
+    Question = True
+    while Question == True:
+      T_to_C()
+      break
   else:
-    print("Please enter the number next to the option you want.")
+    print("Please type in the number next to the conversion that you want.")
     temperature_convert()
+print('Welcome!')
 temperature_convert()
