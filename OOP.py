82a0bf1a8543d93fa629b02275bc354e5eadf5b8
Elton <32944003+EltZ36@users.noreply.github.com>
2020-07-19 18:23:42 -0700

    Create OOP.py
    This is my attempt at trying to make it OOP and one of my first OOP projects. Don't run this file along with the main.py.

---
+++ OOP.py
@@ -0,0 +1,9 @@
+Class Temperature:
+  def __init__(self, type, temperature, convert):
+    self.type = type
+    self.temperature = []
+    self.convert = convert
+    
+  def add_temperature(self, temperature):
+    self.temperature.append(add_temperature)
+  
