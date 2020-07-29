28299aca73eb83d3f3a24011b5a56ef306390b3c
Elton <32944003+EltZ36@users.noreply.github.com>
2020-07-29 14:02:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
 '''
 Goals: -More options
 -More clean and readable code.
+-Try to make sure if people use this, that there is an int check. 
 '''
+
+def Repeat():
+  Again = input("Do you want to convert again? (Y) or (N)")
+  if Again == "Y":
+    temperature_convert()
+  return None
 
 def C_to_F():
   c = int(input("How many degrees Centigrade?"))
   F1 = ( ( (c/5) * 9 ) + 32)
   print(str(F1) + ' degrees Centigrade')
-  Again = input("Do you want to convert again? (Y) or (N)")
-  if Again == "Y":
-    temperature_convert()
-  else:
-    return None
+  Repeat()
 
 def F_to_C():
   f = int(input("How many degrees Fahrenheit?"))
   C1 = ( ( (f - 32) * 5) /9)
   # Have to convert C1 to a string as it is a float and can't be added with another string.
   print(str(C1) + ' degrees Fahrenheit')
-  Again = input("Do you want to convert again? (Y) or (N)")
-  if Again == "Y":
-    temperature_convert()
-  else:
-    return None
+  Repeat()
 
 def C_to_K():
   C2 = int(input("How many degrees Centigrade?"))
   K = (C2 + 273.15)
   print(str(K) + " degrees Kelvin")
-  Again = input("Do you want to convert again? (Y) or (N)")
-  if Again == "Y":
-    temperature_convert()
-  else:
-    return None
+  Repeat()
   
 def K_to_C():
   k1 = int(input("How many degrees Kelvin?"))
   C = (k1 -273.15)
   print(str(C) + " degrees Celsius")
-  Again = input("Do you want to convert again? (Y) or (N)")
-  if Again == "Y":
-    temperature_convert()
-  else:
-    return None
+  Repeat()
   
 def temperature_convert():
   Question = int(input("Do you want to convert from Centigrade to Fahrenheit (1) , Fahrenheit to Centigrade? (2), Centigrade to Kelvin? (3) or Kelvin to Centigrade? (4)"))
