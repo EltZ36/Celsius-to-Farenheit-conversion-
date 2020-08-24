94ce8317d0c2bbc507d6bfd7b26dae2568a3d6ee
Elton <poptako9@gmail.com>
2020-08-24 15:43:55 -0700

    Added rankine conversions
    The rankine conversions are from Rankine to Fahrenheit, Celsius, and
    Kelvin. I also added more onto the dictionary for the conversions and I
    made the string for Question more readable. I did this by breaking it up
    and separating the conversions out. Comments were added to lines 15, 35, 56, and 75 to tell which conversions they are. Makes it more readable and when I come back, I know what they are. A comment was also added to line 8 to explain it more.

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ Goals: -More options
 -Make the temperature conversions more accurate.
 -Reorder the conversions.
 '''
+
+#The repeat function and this allows the person to repeat a conversion again.
 def Repeat():
   Again = input("Do you want to convert again? (Y) or (N)")
   if Again == "Y" or Again == " Y" or Again =="y":
     temperature_convert()
   return None
 
+#Fahrenheit to Celsius, Kelvin, and Rankine conversions
 def F_to_C():
   f = float(input("How many degrees Fahrenheit?"))
   C1 = ( ( (f - 32) * 5) /9)
@@ -23,7 +26,13 @@ def F_to_K():
   print(str(K2) + ' degrees Kelvin')
   Repeat()
 
+def F_to_R():
+  f3 = float(input("How many degrees Fahrenheit?"))
+  R3 = (f3 + 459.67)
+  print(str(R3) + ' degrees Rankine')
+  Repeat()
 
+#Celsius to Fahrenheit, Kelvin, and Rankine conversions
 def C_to_F():
   # float allows for both integers and decimals.
   c = float(input("How many degrees Centigrade?"))
@@ -36,7 +45,15 @@ def C_to_K():
   K = (c2 + 273.15)
   print(str(K) + " degrees Kelvin")
   Repeat()
+
+def C_to_R():
+  c3 = float(input("How many degrees Celsius"))
+  R2 = (((c3)*9/5)+491.67)
+  print(str(R2)+" degrees Rankine")
+  Repeat()
   
+
+#Kelvin to Celsius, Fahrenheit, and Rankine conversions
 def K_to_C():
   k1 = float(input("How many degrees Kelvin?"))
   C2 = (k1 - 273.15)
@@ -49,34 +66,46 @@ def K_to_F():
   print(str(F2) + " degrees Kelvin")
   Repeat()
 
-def F_to_R():
-  f3 = float(input("How many degrees Fahrenheit?"))
-  R1 = (f3 + 459.67)
-  print(str(R1)+" degrees Rankine")
-  Repeat()
-
-def C_to_R():
-  c3 = float(input("How many degrees Celsius"))
-  R2 = (((c3)*9/5)+491.67)
-  print(str(R2)+" degrees Rankine")
-  Repeat()
-
 def K_to_R():
   k3 = float(input("How many degrees Kelvin?"))
   R3 = (k3*1.8)
   print(str(R3)+" degrees Rankine")
   Repeat()
   
+#Rankine to Kelvin, Celsius, and Temperature conversions  
+def R_to_F():
+  r1 = (float(input("How many degrees Rankine?")))
+  F3 = r1 - 459.67
+  print(str(F3)+" degrees Fahrenheit")
+  Repeat()
+
+def R_to_C():
+  r2 = (float(input("How many degrees Rankine?")))
+  C3 = ((r2 -491.67)*5/9)
+  print(str(C3)+" degrees Celsius")
+  Repeat()
+
+def R_to_K():
+  r3 = (float(input("How many degrees Rankine?")))
+  K3 = (r3 * 5/9)
+  print(str(K3)+" degrees Kelvin")
+  Repeat()
+
 def temperature_convert():
  # Lines 55 - 70 check if the input is correct and if it is, then it will convert the temperature. Otherwise if the input(key) isn't found, the while loop will continue.
-  Question = """Which conversion do you want?\nCentigrade to Fahrenheit (1) \nFahrenheit to Centigrade(2) \nCentigrade to Kelvin (3) 
-  \nFahrenheit to Kelvin (4) \nKelvin to Centigrade (5) \nKelvin to Fahrenheit (6) \nFahrenheit to Rankine (7) \nCelsius to Rankine (8) \nKelvin to Rankine (9) """
+  Question = """Which conversion do you want?
+  \n- Centigrade to Fahrenheit (1) - Fahrenheit to Centigrade(2) \n- Centigrade to Kelvin (3) - Fahrenheit to Kelvin (4) 
+  \n-Kelvin to Centigrade (5) - Kelvin to Fahrenheit (6) 
+  \n- Fahrenheit to Rankine (7) - Celsius to Rankine (8) - Kelvin to Rankine (9) 
+  \n- Rankine to Fahrenheit (10) -Rankine to Celsius (11) -Rankine to Kelvin (12)
+  """
   print(Question)
   while True:
    try:
     #using a dictionary to reduce the amount of if statements.
     options ={
-    "1":C_to_F, "2":F_to_C, "3":C_to_K, '4':F_to_K, '5':K_to_C, '6':K_to_F, '7':F_to_R , '8':C_to_R, '9':K_to_R
+    "1":C_to_F, "2":F_to_C, "3":C_to_K, '4':F_to_K, '5':K_to_C, '6':K_to_F, '7':F_to_R , 
+    '8':C_to_R, '9':K_to_R, '10':R_to_F, '11':R_to_C, '12':R_to_K
     }  
     choice = input("Select the number of the conversion here: ")
     options[choice]()
