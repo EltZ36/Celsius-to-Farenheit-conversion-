6be7322fdd97e486bdeb6994ac4160647a96e763
Elton <poptako9@gmail.com>
2020-08-31 14:05:35 -0700

    Filled in rankine conversions
    Added more onto the functions/attributes of the Rankine class. The
    Rankine to Fahrenheit, Celsius, and Kelvin conversions are now added and
    should work as the conversions were added to the dictionary as well. The
    Fahrenheit, Celsius, and Kelvin to Rankine conversions were added as
    well to their respective classes. Added some comments on lines 93 and
    101-102 (mult-line comment). I added in these changes so that there
    would be rankine conversions and the comments were added to sort of
    increase clarity of the project.

--- OOP.py
+++ OOP.py
@@ -35,6 +35,8 @@ class Fahrenheit(Temperature):
     return f'{self.degrees} degrees {self.kind} converts to {x} degrees.'
   def F_to_R(self):
     self.input()
+    x = self.degrees
+    return f'{self.degrees} degrees {self.kind} converts to {x} degrees.'
 
 class Celsius(Temperature):
   def __init__(self, degrees, kind):
@@ -51,6 +53,8 @@ class Celsius(Temperature):
     return f'{self.degrees} degrees {self.kind} converts to {x} degrees.'
   def C_to_R(self):
     self.input()
+    x = self.degrees
+    return f'{self.degrees} degrees {self.kind} converts to {x} degrees.'
 
 class Kelvin(Temperature):
   def __init__(self, degrees, kind):
@@ -67,23 +71,36 @@ class Kelvin(Temperature):
     self.Repeat()
   def K_to_R(self):
     self.input()
+    x = self.degrees 
+    return f'{self.degrees} degrees {self.kind} converts to {x} degrees.'
 
 class Rankine(Temperature):
   def __init__(self, degrees, kind):
     super().__init__(degrees, kind)
   def R_to_F(self):
     self.input()
+    x = (self.degrees - 459.67)
+    return f'{self.degrees} degrees {self.kind} converts to {x} degrees.'
   def R_to_C(self):
     self.input()
+    x = ((self.degrees - 459.67)*5/9)
+    return f'{self.degrees} degrees {self.kind} converts to {x} degrees.'
   def R_to_K(self):
     self.input()
+    x = (self.degrees *5/9)
+    return f'{self.degrees} degrees {self.kind} converts to {x} degrees.'
 
+#I like to call the functions that display the outputs Setup() but you can call it whatever you want. 
 def Setup():
   print("Welcome to the Temperature converter!")
   Response = """What conversion do you want to do? \nFahrenheit to Celsius (1) \nFahrenheit to Kelvin (2) \nCelsius to Fahrenheit (3) 
   \nCelsius to Kelvin (4) \nKelvin to Fahrenheit (5) \nKelvin to Celsius (6)
   """
   print(Response)
+  '''
+  I'm pretty sure these are instances of the classes. 
+  Temp1 is an instance of the Fahrenheit class, Temp2 is an instance of the Celsius class, and so on.
+  '''
   Temp1 = Fahrenheit(0, "Fahrenheit")
   Temp2 = Celsius(0, "Celsius")
   Temp3 = Kelvin(0, "Kelvin")
@@ -92,7 +109,8 @@ def Setup():
    try:
     #using a dictionary to reduce the amount of if statements.
     options ={
-    "1":Temp1.F_to_C, "2":Temp1.F_to_K, "3":Temp2.C_to_F, "4":Temp2.C_to_K, "5":Temp3.K_to_F, "6":Temp3.K_to_C
+    "1":Temp1.F_to_C, "2":Temp1.F_to_K, "3":Temp2.C_to_F, "4":Temp2.C_to_K, "5":Temp3.K_to_F, "6":Temp3.K_to_C, 
+    "7":Temp4.R_to_F, "8":Temp4.R_to_C, "9":Temp4.R_to_K 
     }  
     choice = input("Select the number of the conversion here: ")
     print(options[choice]())
