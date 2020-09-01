2ce737858a7c55e10db52a5391103b1003cc02f2
Elton <poptako9@gmail.com>
2020-09-01 14:46:55 -0700

    Some comments removed
    I removed some of the comments as they seemed to be redundant. Some of the comments for goals are removed as I think that they are done.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 '''
-Goals: -More options
--More clean and readable code.
+Goals: -More clean and readable code.
 -Make the temperature conversions more accurate.
--Reorder the conversions.
 '''
 
-#The repeat function and this allows the person to repeat a conversion again if they type in Y, y or Y with a space.
+
 def Repeat():
   Again = input("Do you want to convert again? (Y) or (N)")
   if Again == "Y" or Again == " Y" or Again =="y":
