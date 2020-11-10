eb3f27e893976fa14dd0a3ccc471bc94c3b8b87b
Elton <32944003+EltZ36@users.noreply.github.com>
2020-11-09 19:16:09 -0800

    Change temperature print statements
    I changed the print statements of the temperature functions to f
    strings. This makes it better and I can change the variables better. So
    that it says for example 32 degrees fahrenheit (the temperature about to
    be converted) to 0 degrees celsius (the temperature you want converted).
    The variable temp_type is global and is used for the temperature type
    that is about to be converted. I also changed '' to "" just for
    organization and to be consistent. There is no difference between '' and
    "" in Python. I made these changes after checking it and realizing that
    I can make these changes.

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ Goals: -More clean and readable code.
 -Make the temperature conversions more accurate.
 '''
 
+temp_type= " "
+
 def Repeat():
   while True:
     try:
       Again = input("Do you want to convert again? (Y) or (N)")
-      Options = {"Y":temperature_convert, "y":temperature_convert, " y":temperature_convert, "Y":temperature_convert, "N":exit, "n":exit}
+      Options = {"Y":temperature_convert, "y":temperature_convert, " y":temperature_convert, 
+      "Y":temperature_convert, "N":exit, "n":exit}
       print(Options[Again]())
     #Keyerror occurs when the input/key isn't found in the dictionary.
     except KeyError:
@@ -16,85 +19,109 @@ def Repeat():
 
 #Fahrenheit to Centigrade, Kelvin, and Rankine conversions
 def F_to_C():
+  global temp_type 
+  temp_type = "Fahrenheit"
   f = float(input("How many degrees Fahrenheit?"))
   C1 = ((f - 32) * (5/9))
   # Float allows for both integers and decimals.
   # Have to convert this(C1) into a string as it is a float and can't be added with another string.
-  print(str(C1) + ' degrees Fahrenheit')
+  print(f"{str(f)} degrees {temp_type} converts to {str(C1)} degrees Celsius")
   Repeat()
   
 def F_to_K():
+  global temp_type 
+  temp_type = "Fahrenheit"
   f2 = float(input("How many degrees Fahrenheit?"))
   K2 = (((f2 -32) * 5/9) + 273.15)
-  print(str(K2) + ' Kelvin')
+  print(f"{str(f2)} degrees {temp_type} converts to {str(K2)} Kelvin")
   Repeat()
 
 def F_to_R():
+  global temp_type 
+  temp_type = "Fahrenheit"
   f3 = float(input("How many degrees Fahrenheit?"))
   R3 = (f3 + 459.67)
-  print(str(R3) + ' degrees Rankine')
+  print(f"{str(f3)} degrees {temp_type} converts to {str(R3)} degrees Rankine")
   Repeat()
 
 #Centigrade to Fahrenheit, Kelvin, and Rankine conversions
 def C_to_F():
+  global temp_type 
+  temp_type = "Centigrade"
   c = float(input("How many degrees Centigrade?"))
   F1 = (c *(9/5) + 32)
-  print(str(F1) + ' degrees Centigrade')
+  print(f"{str(c)} degrees {temp_type} converts to {str(F1)} degrees Fahrenheit")
   Repeat()
 
 def C_to_K():
+  global temp_type 
+  temp_type = "Centigrade"
   c2 = float(input("How many degrees Centigrade?"))
   K = (c2 + 273.15)
-  print(str(K) + " Kelvin")
+  print(f"{str(c2)} degrees {temp_type} converts to {str(K)} Kelvin")
   Repeat()
 
 def C_to_R():
+  global temp_type 
+  temp_type = "Centigrade"
   c3 = float(input("How many degrees Centigrade"))
   R2 = (((c3) * 9/5 ) + 491.67)
-  print(str(R2)+" degrees Rankine")
+  print(f"{str(c3)} degrees {temp_type} converts to {str(R2)} degrees Rankine")
   Repeat()
   
 
 #Kelvin to Centigrade, Fahrenheit, and Rankine conversions
 def K_to_C():
+  global temp_type 
+  temp_type = "Kelvin"
   k1 = float(input("How much Kelvin?"))
   C2 = (k1 - 273.15)
-  print(str(c2) + " degrees Centigrade")
+  print(f"{str(k1)} {temp_type} converts to {str(C2)} degrees Celsius")
   Repeat()
 
 def K_to_F():
+  global temp_type 
+  temp_type = "Kelvin"
   k2 = float(input("How much Kelvin?"))
   F2 = (((k2 - 273.15) * 9/5) + 32) 
-  print(str(F2) + " Kelvin")
+   print(f"{str(k2)} {temp_type} converts to {str(F2)} degrees Fahrenheit")
   Repeat()
 
 def K_to_R():
+  global temp_type 
+  temp_type = "Kelvin"
   k3 = float(input("How much Kelvin?"))
   R3 = (k3 * 1.8)
-  print(str(R3)+" degrees Rankine")
+  print(f"{str(k3)} {temp_type} converts to {str(R3)} degrees Rankine")
   Repeat()
   
 #Rankine to Kelvin, Celsius, and Temperature conversions  
 def R_to_F():
+  global temp_type 
+  temp_type = "Rankine"
   r1 = (float(input("How many degrees Rankine?")))
   F3 = r1 - 459.67
-  print(str(F3)+" degrees Fahrenheit")
+  print(f"{str(r1)} degrees {temp_type} converts to {str(F3)} degrees Fahrenheit")
   Repeat()
 
 def R_to_C():
+  global temp_type 
+  temp_type = "Rankine"
   r2 = (float(input("How many degrees Rankine?")))
   C3 = ((r2 - 491.67) * 5/9)
-  print(str(C3)+" degrees Centigrade")
+  print(f"{str(r2)} degrees {temp_type} converts to {str(C3)} degrees Celsius")
   Repeat()
 
 def R_to_K():
+  global temp_type 
+  temp_type = "Rankine"
   r3 = (float(input("How many degrees Rankine?")))
   K3 = (r3 * 5/9)
-  print(str(K3)+" Kelvin")
+  print(f"{str(r3)} degrees {temp_type} converts to {str(K3)} Kelvin")
   Repeat()
 
 def temperature_convert():
- # Lines 55 - 70 check if the input is correct and if it is, then it will convert the temperature. Otherwise if the input(key) isn't found, the while loop will continue.
+ # Lines 133 - 144 (or 134-145) check if the input is correct and if it is, then it will convert the temperature. Otherwise if the input(key) isn't found, the while loop will continue.
   Question = """Which conversion do you want?
   \n- Centigrade to Fahrenheit (1) - Fahrenheit to Centigrade(2) 
   \n- Centigrade to Kelvin (3) - Fahrenheit to Kelvin (4) 
@@ -106,8 +133,8 @@ def temperature_convert():
   while True:
    try:
     options ={
-    "1":C_to_F, "2":F_to_C, "3":C_to_K, '4':F_to_K, '5':K_to_C, '6':K_to_F, '7':F_to_R , 
-    '8':C_to_R, '9':K_to_R, '10':R_to_F, '11':R_to_C, '12':R_to_K
+    "1":C_to_F, "2":F_to_C, "3":C_to_K, "4":F_to_K, "5":K_to_C, "6":K_to_F, "7":F_to_R , 
+    "8":C_to_R, "9":K_to_R, "10":R_to_F, "11":R_to_C, "12":R_to_K
     }  
     choice = input("Select the number of the conversion here: ")
     print(options[choice]())
@@ -115,5 +142,5 @@ def temperature_convert():
    except KeyError:
     print("Sorry, please type in the number of the conversion again")
     continue 
-print('Welcome to the Temperature Converter!')
+print("Welcome to the Temperature Converter!")
 temperature_convert()
